fe28f834be4f67b2f590817d5d3c2a97562ae228
Shwetha K <shweetha.sweety17@gmail.com>
2024-11-23 18:22:24 +0530

    24-11-23_Personal_Budget_Manager[shwetha k]: integrating frontend react with backend framework flask

---
+++ backend/app.py
@@ -0,0 +1,35 @@
+from flask import Flask,redirect,render_template,request,request,url_for
+from flask_mysqldb import MySQL
+import MySQLdb.cursors 
+import re 
+
+
+app=Flask(__name__)
+app.secret_key="secret_key_app"
+
+# app configuration
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='root321'
+app.config['MYSQL_DB']='personal_budget_manager'
+
+mysql=MySQL(app)
+
+@app.route("/")
+@app.route('/login',methods=['GET','POST'])
+def main():
+    msg=''
+    if request.method=='POST' and 'username' in request.form and 'password' in request.form:
+       
+        cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select * from user')
+        account=cursor.fetchone()
+        # testing
+        # print(account)
+        return render_template('login_success.html')
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.debug=True 
+    app.run()
